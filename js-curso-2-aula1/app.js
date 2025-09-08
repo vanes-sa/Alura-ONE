4f552ccec315f6091043b414cb94a19204197b46
VanessaSantos <vanessa.ufs2022@gmail.com>
2025-09-08 20:48:44 -0300

    update

--- js-curso-2-aula1/app.js
+++ js-curso-2-aula1/app.js
@@ -1,9 +1,19 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número de 1 a 10';
+let numeroSecreto = gerarNumeroAleatorio();
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número de 1 a 10');
 
 function verificarChute() {
-    console.log('Botão chutar clicado');
+    let chute = document.querySelector('.container__input').value;
+    console.log(chute == numeroSecreto);
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10) + 1;
+
 }
