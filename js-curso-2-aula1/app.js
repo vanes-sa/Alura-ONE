01adf927f1935a5fe9476f03438b0963bbea7321
VanessaSantos <vanessa.ufs2022@gmail.com>
2025-09-22 00:00:23 -0300

    Update app.js
    refatoração da função que realiza o sorteio do número secreto, tornando-a mais robusta e garantindo que o número sorteado não seja repetido

--- js-curso-2-aula1/app.js
+++ js-curso-2-aula1/app.js
@@ -1,4 +1,5 @@
-
+let listaDeNumerosSorteados = [];
+let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -10,7 +11,7 @@ function mensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
     exibirTextoNaTela('p', 'Escolha um número de 1 a 10:');
 }
-mensagemInicial();
+mensagemInicial(); 
 
 function verificarChute() {
     let chute = document.querySelector('.container__input').value;
@@ -32,8 +33,19 @@ function verificarChute() {
 }
 
 function gerarNumeroAleatorio() {
-    return parseInt(Math.random() * 10) + 1;
-
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+    if(quantidadeDeElementosNaLista == numeroLimite) {
+        listaDeNumerosSorteados = [];
+    }
+    
+    
+    if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
+        return gerarNumeroAleatorio();
+    } else {
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        return numeroEscolhido;
+    }
 }
 
 function limparCampo() {
